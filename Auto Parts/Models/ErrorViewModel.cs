977292c33ac6100459ed7de38f7bb33679d201d8
HP <HP@DESKTOP-HHKMTTK>
2024-05-29 00:08:54 +0500

    Add project files.

---
+++ Auto Parts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Auto_Parts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
